81a408d1512846072a63ac4f1ef8e382c7088161
silambuayyakk <42801305+silambuayyakk@users.noreply.github.com>
2018-10-04 13:10:05 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DealsComponent } from './deals/deals.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path : 'login', component: LoginComponent
+},
+  { 
+    path : 'register', component: RegisterComponent
+  },
+  { 
+    path : 'deals', component: DealsComponent
+  },
+  { 
+    path : 'post', component: PostComponent
+  }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents= [LoginComponent,RegisterComponent,DealsComponent,PostComponent]
