3f99bb8da142d357c83995e5d957f9094dcf41db
Abinaya_eBrain <39687458+AbinayaEbrain@users.noreply.github.com>
2018-11-12 05:57:58 -0500

    with single index.html

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use(cors())
 app.use('/api',api)
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/farmers'));
+app.use(express.static(___dirname + '/dist/farmers'));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+ '/dist/farmers/index.html'));
+app.get('*', function(req,res) {
+    res.sendFile(path.join(___dirname+ '/dist/farmers/index.html'));
     });
 
 // Start the app by listening on the default Heroku port
