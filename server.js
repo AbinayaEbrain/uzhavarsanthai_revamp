5a1a4e37b696d5849339b7b2f26f4d9c07b46f57
Abinaya_eBrain <39687458+AbinayaEbrain@users.noreply.github.com>
2018-11-13 07:09:50 -0500

    the end 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser =require('body-parser')
 const cors=require('cors')
 const path = require('path');
-const api = require('./routes/api')
+const api = require('./server/routes/api')
 
 const app = express()
 
@@ -13,10 +13,10 @@ app.use(cors())
 app.use('/api',api)
 
 // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/dist/farmers'));
+ app.use(express.static(__dirname + './dist/farmers'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+ '/dist/farmers/index.html'));
+    res.sendFile(path.join(__dirname+ './dist/farmers/index.html'));
     });
 
 
