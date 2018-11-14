de45e5ed0614a467f3ca7689c68a5d6aff9130bc
Abinaya_eBrain <39687458+AbinayaEbrain@users.noreply.github.com>
2018-11-14 00:31:23 -0500

    14 mor

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser =require('body-parser')
 const cors=require('cors')
 const path = require('path');
-const api = require('./server/routes/api')
+const api = require('../server/routes/api')
 
 const app = express()
 
