270d836431a75e901bc4d756b3e4f07bae0cacce
Abinaya_eBrain <39687458+AbinayaEbrain@users.noreply.github.com>
2018-11-14 04:42:39 -0500

    with ..

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use('/api',api)
  app.use(express.static(__dirname + '/farmers'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+ '/index.html'));
+    res.sendFile(path.join(__dirname+ '/../index.html'));
     });
 
 
