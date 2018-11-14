74e68e719c3b2bc84bea03ed5e4ee302d074fbd7
Abinaya_eBrain <39687458+AbinayaEbrain@users.noreply.github.com>
2018-11-14 07:33:49 -0500

    hello

--- server/server.js
+++ server/server.js
@@ -13,14 +13,15 @@ app.use(cors())
 app.use('/api',api)
 
 // Serve only the static files form the dist directory
- app.use(express.static(__dirname + 'farmers'));
+ app.use(express.static(__dirname + '/src'));
+
+// app.get('', function(req,res) {
+//     res.sendFile(path.join(__dirname + '/app/index.html'));
+//     });
 
 app.get('', function(req,res) {
-    res.sendFile(path.join(__dirname + '/app/index.html'));
-    });
-
-
-
+    res.send("Hello");
+ });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);
 
