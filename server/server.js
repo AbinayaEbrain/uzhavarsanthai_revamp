b02037995b4650148dbc2dd1491794ab55697d34
Abinaya_eBrain <39687458+AbinayaEbrain@users.noreply.github.com>
2018-11-14 06:24:39 -0500

    with full pat1

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use(cors())
 app.use('/api',api)
 
 // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/farmers'));
+ app.use(express.static(__dirname + '/dist/farmers'));
 
-app.get('/', function(req,res) {
-    res.sendFile(path.join('farmers/src/app', 'index.html'));
+app.get('', function(req,res) {
+    res.sendFile(path.join(__dirname + '/dist/farmers/index.html'));
     });
 
 
