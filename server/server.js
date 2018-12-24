e683693e666264c86db4b4aee61caaefdb732786
eBrainTechnologies <ulaganathank@ebraintechnologies.com>
2018-12-24 17:06:55 +0530

    heroku up

--- server/server.js
+++ server/server.js
@@ -65,10 +65,10 @@ app.post('/api/upload',upload.single('photo'), function (req, res) {
 });
 
     // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/farmers'));
+ app.use(express.static(__dirname + '/src'));
 
  app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname+ '/farmers/index.html'));
+    res.sendFile(path.join(__dirname+ '/src/index.html'));
     });
 
     
@@ -76,6 +76,6 @@ app.post('/api/upload',upload.single('photo'), function (req, res) {
 //     res.send("Hello");
 //  });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
 
 
