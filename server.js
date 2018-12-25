f7ce46b1c323a0bb4d42884335960d858e92166c
eBrainTechnologies <ulaganathank@ebraintechnologies.com>
2018-12-25 13:09:44 +0530

    outside server file

--- server.js
+++ server.js
@@ -67,18 +67,17 @@ app.post('/api/upload',upload.single('photo'), function (req, res) {
 });
 
     // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/src'));
+//  app.use(express.static(__dirname + '/src'));
+app.use(express.static(path.join(__dirname, '/dist')));
 
- app.get('/*', function(req,res) {
-    res.sendFile(path.join((__dirname + '/src/index.html')));
-    });
 
-// app.use(express.static(__dirname + '/dist/farmers'));
+//  app.get('/*', function(req,res) {
+//     res.sendFile(path.join((__dirname + '/src/index.html')));
+//     });
 
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/farmers/index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/farmers/index.html'));
+});
 
     
 // app.get('/', function(req,res) {
