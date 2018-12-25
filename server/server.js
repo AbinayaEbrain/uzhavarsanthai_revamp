8d60e1160b0395ce2785e5555b6f348d3d736a68
eBrainTechnologies <ulaganathank@ebraintechnologies.com>
2018-12-25 12:14:05 +0530

    25th 8th

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use('/api',api)
 
-console.log(__dirname)
+console.log(__dirname , '/src')
 
 // Serve only the static files form the dist directory
 //  app.use(express.static('/farmers'));
@@ -67,10 +67,10 @@ app.post('/api/upload',upload.single('photo'), function (req, res) {
 });
 
     // Serve only the static files form the dist directory
- app.use(express.static(__dirname , '../src'));
+ app.use(express.static(__dirname + '/src'));
 
  app.get('/*', function(req,res) {
-    res.sendFile(path.join((__dirname , '../src/index.html')));
+    res.sendFile(path.join((__dirname + '/src/index.html')));
     });
 
 // app.use(express.static(__dirname + '/dist/farmers'));
