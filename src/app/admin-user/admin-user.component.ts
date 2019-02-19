2d9fe29482d68d8c4fa355cd82f3e58ec21c0e2f
Ulaganathan <39687458+AbinayaEbrain@users.noreply.github.com>
2019-02-19 18:48:19 +0530

    meta

--- src/app/admin-user/admin-user.component.ts
+++ src/app/admin-user/admin-user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DealsService } from '../deals.service';
-import {ActivatedRoute} from '@angular/router';
-import {Router, ParamMap} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Router, ParamMap } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
@@ -10,107 +10,112 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./admin-user.component.css']
 })
 export class AdminUserComponent implements OnInit {
+  //  public registerUser=[{
+  //     firstname:'',
+  //     _id:'',
+  //     status:'',
+  //     address:{
+  //       city:'',
+  //       location:''
+  //     }
+  //   }
+  //   ]
 
-  registerUser=[{
-    firstname:'',
-    _id:'',
-    status:'',
-    address:{
-      city:'',
-      location:''
-    }
-  }
-  ]
+  public registerUser: any = [];
 
-  mapDeals=[];
-  id:any
-errMsg:any;
-deactiveMsg:any
-activeMsg:any
-p:any;
-data:any;
+  mapDeals = [];
+  id: any;
+  errMsg: any;
+  deactiveMsg: any;
+  activeMsg: any;
+  p: any;
+  data: any;
 
-  constructor(private _dealService:DealsService,private route:ActivatedRoute,private router:Router,public loadingCtrl: NgxSpinnerService
-  ) { }
+  constructor(
+    private _dealService: DealsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public loadingCtrl: NgxSpinnerService
+  ) {}
 
   ngOnInit() {
-
     this.loadingCtrl.show();
-    this._dealService.getDetails()
-       .subscribe(
-         res=>{
-          this.loadingCtrl.hide();
-          this.registerUser= res;
-            for(let i=0;i<this.registerUser.length;i++){
-              if(this.registerUser[i].firstname == "Admin"){
-                this.registerUser.splice(this.registerUser.indexOf(this.registerUser[i]), 1);
-               }
-            }
-         
-
-          if(this.registerUser.length == 0){
-            this.errMsg = "No users found"
-            }
-         },
-         err=>{
-          this.loadingCtrl.hide();
-          console.log(err)
-         }
-       )
-
+    this._dealService.getDetails().subscribe(
+      res => {
+        this.loadingCtrl.hide();
+        this.registerUser = res;
+        console.log(this.registerUser);
+        for (let i = 0; i < this.registerUser.length; i++) {
+          if (this.registerUser[i].firstname == 'Admin') {
+            this.registerUser.splice(
+              this.registerUser.indexOf(this.registerUser[i]),
+              1
+            );
+          }
+          if (this.registerUser[i].address.city.formatted_address) {
+            let city = this.registerUser[i].address.city.formatted_address;
+            this.registerUser[i].address.city.formatted_address = city.split(
+              ','
+            )[0];
+            this.registerUser[i].address.city.locality = city.split(',')[1];
+          }
+        }
+        if (this.registerUser.length == 0) {
+          this.errMsg = 'No users found';
+        }
+      },
+      err => {
+        this.loadingCtrl.hide();
+        console.log(err);
+      }
+    );
   }
 
   //Deactivate Account
-  deactiveAccount(data,id){
-    this.id = this.route.snapshot.params['id']
-    this._dealService.deactivateAccount(data,this.id)
-    .subscribe(
-       res=>{ 
-         this.deactiveMsg ='Account Deactivated!'
-            setTimeout(()=>{
-              this.deactiveMsg = ''
-              this.router.navigate[('/admin-user')]
-            },2000)
-           // this.router.navigate[('/admin-user')]
-       },
-       err=>{ console.log(err);
+  deactiveAccount(data, id) {
+    this.id = this.route.snapshot.params['id'];
+    this._dealService.deactivateAccount(data, this.id).subscribe(
+      res => {
+        this.deactiveMsg = 'Account Deactivated!';
+        setTimeout(() => {
+          this.deactiveMsg = '';
+          this.router.navigate['/admin-user'];
+        }, 2000);
+        // this.router.navigate[('/admin-user')]
       },
-    
-    )
+      err => {
+        console.log(err);
+      }
+    );
   }
 
-   //activate Account
-  activeAccount(data,id){
-    this.id = this.route.snapshot.params['id']
-    this._dealService.activateAccount(data,this.id)
-    .subscribe(
-       res=>{ 
-         this.activeMsg ='Account Activated!'
+  //activate Account
+  activeAccount(data, id) {
+    this.id = this.route.snapshot.params['id'];
+    this._dealService.activateAccount(data, this.id).subscribe(
+      res => {
+        this.activeMsg = 'Account Activated!';
         //  alert(this.activeMsg)
-         setTimeout(()=>{
-          this.activeMsg = ''
-          this.router.navigate[('/admin-user')]
-         },2000)
-        
-       },
-       err=>{ console.log(err);
+        setTimeout(() => {
+          this.activeMsg = '';
+          this.router.navigate['/admin-user'];
+        }, 2000);
       },
-    
-    )
+      err => {
+        console.log(err);
+      }
+    );
   }
 
-  deleteuser(){
-    this.id = this.route.snapshot.params['id']
-    this._dealService.deleteUser(this.id)
-    .subscribe(
-       res=>{ console.log(res)
-       
-       
-       },
-       err=>{ console.log(err);
+  deleteuser() {
+    this.id = this.route.snapshot.params['id'];
+    this._dealService.deleteUser(this.id).subscribe(
+      res => {
+        console.log(res);
       },
-    
-    )
+      err => {
+        console.log(err);
+      }
+    );
   }
-
 }
--- src/app/admin-view/admin-view.component.ts
+++ src/app/admin-view/admin-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DealsService } from '../deals.service';
-import { Router} from '@angular/router';
-import {ActivatedRoute, Params} from '@angular/router'
+import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
@@ -10,102 +10,86 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./admin-view.component.css']
 })
 export class AdminViewComponent implements OnInit {
-
-  id:any
-  postProduct =[]
+  id: any;
+  postProduct = [];
   viewPost = [];
-  productId:any
-  descrip:any
-  registerUser = []
-  register = {
-    firstName : '',
-    lastName:'',
-    gender:'',
-    phone:'',
-    privateIP:'',
-    address:{
-      addressLine:'',
-      address1:'',
-      city:{
-        locality:'',
-        admin_area_l1:''
-      },
-      location:'',
-    }
-  }
-  postLength:any
-  errMsg:any;
-  p:any;
-  queryString:any;
-  locality:any
-  admin_area_l1:any
+  productId: any;
+  descrip: any;
+  public registerUser: any = [];
+  public register: any = {};
+  postLength: any;
+  errMsg: any;
+  p: any;
+  queryString: any;
+  locality: any;
+  admin_area_l1: any;
+  city: any;
 
-
-  constructor(private _dealsService:DealsService,private router:Router,private route:ActivatedRoute,public loadingCtrl: NgxSpinnerService) { }
+  constructor(
+    private _dealsService: DealsService,
+    private router: Router,
+    private route: ActivatedRoute,
+    public loadingCtrl: NgxSpinnerService
+  ) {}
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
 
-    this.id = this.route.snapshot.params['id']
-
-    this._dealsService.getDetails()
-    .subscribe(
-      res=>{
-       this.loadingCtrl.hide();
-       this.registerUser= res;
-       for(let i=0;i<this.registerUser.length;i++){
-       if(this.id == this.registerUser[i]._id){
-        this.register.firstName =  this.registerUser[i].firstname;
-        this.register.lastName =  this.registerUser[i].lastName;
-        this.register.gender =  this.registerUser[i].gender;
-        this.register.phone = this.registerUser[i].phone;
-        this.register.address.addressLine = this.registerUser[i].address.addressLine;
-        this.register.address.address1 = this.registerUser[i].address.address1;
-        this.register.address.city = this.registerUser[i].address.city;
-        this.register.address.location = this.registerUser[i].address.location;
-        this.register.privateIP =this.registerUser[i].privateIP;
-       }
-      }
-      },
-      err=>{
-        console.log(err)
-      }
-    )
-
-
-    this._dealsService.getDeals()
-    .subscribe(
-      res=>{
+    this._dealsService.getDetails().subscribe(
+      res => {
         this.loadingCtrl.hide();
-        this.viewPost = res
-       let j=0
-        for(let i=0; i < this.viewPost.length; i++){
-          if(this.id == this.viewPost[i].accountId){
-            this.postProduct[j] =  this.viewPost[i];
-            //length
-            this.productId = this.postProduct[j]._id
-            j++
+        this.registerUser = res;
+        console.log(this.registerUser);
+        for (let i = 0; i < this.registerUser.length; i++) {
+          if (this.id == this.registerUser[i]._id) {
+            this.register.firstName = this.registerUser[i].firstname;
+            this.register.lastName = this.registerUser[i].lastName;
+            this.register.gender = this.registerUser[i].gender;
+            this.register.phone = this.registerUser[i].phone;
+            this.register.address = this.registerUser[i].address.addressLine;
+            this.city = this.registerUser[i].address.city.formatted_address;
+            this.register.privateIP = this.registerUser[i].privateIP;
           }
         }
-        this.postLength = this.postProduct.length
-      
-        if(this.postLength == 0){
-          this.errMsg = "No posts found";
+        this.register.city = this.city.split(',')[0];
+        this.register.state = this.city.split(',')[1];
+      },
+      err => {
+        console.log(err);
+      }
+    );
+
+    this._dealsService.getDeals().subscribe(
+      res => {
+        this.loadingCtrl.hide();
+        this.viewPost = res;
+        let j = 0;
+        for (let i = 0; i < this.viewPost.length; i++) {
+          if (this.id == this.viewPost[i].accountId) {
+            this.postProduct[j] = this.viewPost[i];
+            //length
+            this.productId = this.postProduct[j]._id;
+            j++;
+          }
+        }
+        this.postLength = this.postProduct.length;
+
+        if (this.postLength == 0) {
+          this.errMsg = 'No posts found';
         }
       },
-      err=> console.log(err)
-    )
-
+      err => console.log(err)
+    );
   }
 
-  modalDes(_id){
+  modalDes(_id) {
     // alert(this.productId)
     // alert("1")
     // alert(_id)
-    for(let i=0; i < this.postProduct.length; i++){
-      if(_id == this.postProduct[i]._id){
-        this.descrip =  this.postProduct[i].description;
+    for (let i = 0; i < this.postProduct.length; i++) {
+      if (_id == this.postProduct[i]._id) {
+        this.descrip = this.postProduct[i].description;
       }
     }
   }
-
 }
