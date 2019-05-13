be7c6fe05fb1aa55550d8c923215bfe2e6d7d278
Ulaganathan <39687458+AbinayaEbrain@users.noreply.github.com>
2019-05-13 11:06:43 +0530

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,6 +59,7 @@ import { MyOrderComponent } from './my-order/my-order.component';
 import { DummyComponent } from './dummy/dummy.component';
 import { CreditsComponent } from './credits/credits.component';
 import { AdminDisputeComponent } from './admin-dispute/admin-dispute.component';
+import { DisputeMoreComponent } from './dispute-more/dispute-more.component';
 
 @NgModule({
   declarations: [
@@ -95,7 +96,8 @@ import { AdminDisputeComponent } from './admin-dispute/admin-dispute.component';
     SellerOrderRequestsComponent,
     MyOrderComponent,
     CreditsComponent,
-    AdminDisputeComponent
+    AdminDisputeComponent,
+    DisputeMoreComponent
   ],
   imports: [
     BrowserModule,
