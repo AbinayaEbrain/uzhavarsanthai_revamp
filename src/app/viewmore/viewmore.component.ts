5b4b0923bb195be4de8719aaf4afee4bc29d1f0b
Ulaganathan <39687458+AbinayaEbrain@users.noreply.github.com>
2019-06-07 11:37:26 +0530

    last

--- src/app/viewmore/viewmore.component.ts
+++ src/app/viewmore/viewmore.component.ts
@@ -920,6 +920,10 @@ mapWithPost(){
       }
     }
 
+    closeReset(){
+      this.mytemplateForm3.reset();
+    }
+
     handleInput(evt) {
       var charCode = evt.which ? evt.which : evt.keyCode;
       if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
