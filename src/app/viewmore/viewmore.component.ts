f5eaf5ebd4a359e135c3ddddfa179216601f7b3b
Ulaganathan <39687458+AbinayaEbrain@users.noreply.github.com>
2019-06-07 21:30:44 +0530

    comment viewmore code

--- src/app/viewmore/viewmore.component.ts
+++ src/app/viewmore/viewmore.component.ts
@@ -372,18 +372,18 @@ postQuery(){
   
   this.buyerName = JSON.parse(localStorage.getItem('currentUser')).firstname;
   this.buyerPhone = JSON.parse(localStorage.getItem('currentUser')).phone;
-  if(localStorage.getItem('currentUpdateAddr') == null || localStorage.getItem('currentUpdateAddr') == undefined){
-    this.buyerAddress = JSON.parse(localStorage.getItem('currentUser')).address.addressLine;
-    this.buyerCity = JSON.parse(localStorage.getItem('currentUser')).address.city.formatted_address;
-  }else{
-    this.buyerAddress = JSON.parse(localStorage.getItem('currentUpdateAddr')).address.addressLine;
-    this.buyerCity = JSON.parse(localStorage.getItem('currentUpdateAddr')).address.city.formatted_address;
-  }
+  // if(localStorage.getItem('currentUpdateAddr') == null || localStorage.getItem('currentUpdateAddr') == undefined){
+  //   this.buyerAddress = JSON.parse(localStorage.getItem('currentUser')).address.addressLine;
+  //   this.buyerCity = JSON.parse(localStorage.getItem('currentUser')).address.city.formatted_address;
+  // }else{
+  //   this.buyerAddress = JSON.parse(localStorage.getItem('currentUpdateAddr')).address.addressLine;
+  //   this.buyerCity = JSON.parse(localStorage.getItem('currentUpdateAddr')).address.city.formatted_address;
+  // }
  
   this.querydata.buyerName =  this.buyerName;
   this.querydata.buyerPhone =  this.buyerPhone;
-  this.querydata.buyerAddress =  this.buyerAddress;
-  this.querydata.buyerCity =  this.buyerCity;
+  // this.querydata.buyerAddress =  this.buyerAddress;
+  // this.querydata.buyerCity =  this.buyerCity;
   this.querydata.buyerId = JSON.parse(localStorage.getItem('currentUser'))._id;
   //seller data
   this.querydata.sellerName = this.postProduct.firstName;
